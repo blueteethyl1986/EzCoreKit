67576882a3a16f7bb1af6ac3c9c7a0d6c3df9b34
徐培堯 <st89382000@gmail.com>
2017-04-28 09:42:45 +0800

    加入AspNetCore MVC擴充功能

---
+++ EzCoreKit/Microsoft/AspNetCore/Mvc/DisableAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.AspNetCore.Mvc {
+    /// <summary>
+    /// 標誌停用的控制器或方法
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class DisableAttribute : Attribute {
+        
+    }
+}
