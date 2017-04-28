66bd6b3ba872534f381b93e8e7c685e1e0372df3
徐培堯 <st89382000@gmail.com>
2017-04-28 09:12:00 +0800

    加入System常用工廠與擴充方法

---
+++ EzCoreKit/System/Extensions/StringExtension.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace EzCoreKit.System.Extensions {
+    public static class StringExtension {
+        /// <summary>
+        /// 使用正規表示式切割字串
+        /// </summary>
+        /// <param name="input">值</param>
+        /// <param name="pattern">模式</param>
+        /// <returns>切割結果</returns>
+        public static string[] SplitByRegex(this string input, string pattern) {
+            return new Regex(pattern).Split(input);
+        }
+
+        /// <summary>
+        /// 使用正規表示式切割字串
+        /// </summary>
+        /// <param name="input">值</param>
+        /// <param name="pattern">模式</param>
+        /// <param name="count">數量</param>
+        /// <returns>切割結果</returns>
+        public static string[] SplitByRegex(this string input, string pattern,int count) {
+            return new Regex(pattern).Split(input,count);
+        }
+
+        /// <summary>
+        /// 使用正規表示式切割字串
+        /// </summary>
+        /// <param name="input">值</param>
+        /// <param name="pattern">模式</param>
+        /// <param name="count">數量</param>
+        /// <param name="startat">起始索引</param>
+        /// <returns>切割結果</returns>
+        public static string[] SplitByRegex(this string input, string pattern,int count,int startat) {
+            return new Regex(pattern).Split(input,count,startat);
+        }
+
+        /// <summary>
+        /// 取得指定字串間的字串
+        /// </summary>
+        /// <param name="input">值</param>
+        /// <param name="start">起始字串</param>
+        /// <param name="end">結束字串</param>
+        /// <returns>字串間的字串</returns>
+        public static string InnerString(this string input, string start, string end) {
+            string result = input.Substring(input.IndexOf(start) + start.Length);
+            return result.Substring(0, result.IndexOf(end));
+        }
+    }
+}
---
+++ EzCoreKit/System/Security/Cryptography/Extensions/StringHashExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace EzCoreKit.System.Security.Cryptography {
+    public static class StringHashExtension {
+        /// <summary>
+        /// 將字串使用指定的雜湊演算法轉換為雜湊
+        /// </summary>
+        /// <typeparam name="Algorithm">雜湊演算法型別</typeparam>
+        /// <param name="value">值</param>
+        /// <returns>雜湊Binary</returns>
+        public static byte[] ToHash<Algorithm>(this string value) where Algorithm : HashAlgorithm {
+            return HashFactory.ToHash<Algorithm>(value);
+        }
+
+        /// <summary>
+        /// 將字串使用指定的雜湊演算法轉換為雜湊後在轉換為16進位字串表示
+        /// </summary>
+        /// <typeparam name="Algorithm">雜湊演算法型別</typeparam>
+        /// <param name="value">值</param>
+        /// <param name="upper">是否轉換為大寫</param>
+        /// <returns>雜湊字串</returns>
+        public static string ToHashString<Algorithm>(this string value, bool upper = true) where Algorithm : HashAlgorithm {
+            return HashFactory.ToHashString<Algorithm>(value, upper);
+        }
+    }
+}
