c5ed3856baf6d9efd5fcae83875ceb58d54df615
徐培堯 <st89382000@gmail.com>
2017-05-15 14:46:31 +0800

    加入HttpContext與EF Options取得支援

---
+++ EzCoreKit/Microsoft/EntityFrameworkCore/Extensions/DbContextExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace EzCoreKit.Microsoft.EntityFrameworkCore.Extensions {
+    public static class DbContextExtension {
+        public static DbContextOptions<T> GetOptions<T>(this T dbcontext) where T:DbContext {
+            var options = dbcontext.GetType().GetTypeInfo().BaseType.GetField("_options", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(dbcontext);
+            return options as DbContextOptions<T>;
+        }
+            
+
+    }
+}
