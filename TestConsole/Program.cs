b89637206c7423de1243d5e17977f438f3efa93b
徐培堯 <st89382000@gmail.com>
2017-06-02 13:03:52 +0800

    加入網路爬蟲基礎庫專案

---
+++ TestConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace TestConsole {
+    class Program {
+        static void Main(string[] args) {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            Console.InputEncoding = Encoding.GetEncoding(0);
+            Console.OutputEncoding = Encoding.GetEncoding(0);
+
+            int i = 0;
+            foreach(var news in new NkfustNews()) {
+                if (i++ == 10) break;
+                Console.WriteLine($"{news.Date} - {news.Author} - {news.Title}");
+                if (!news.NoContent) {
+                    Console.WriteLine(news.ContentText);
+                }
+                Console.WriteLine("==========");
+            }            
+        }
+    }
+}
