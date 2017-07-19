0f2a31a00dafa19365308644904b4b6d51b68f2a
徐培堯 <st89382000@gmail.com>
2017-07-19 15:20:14 +0800

    Add Let's Encrypt command line tools

---
+++ EzCoreKit.LetsEncrypt.Tools/Options.cs
@@ -0,0 +1,24 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EzCoreKit.LetsEncrypt.Tools {
+    public class Options {
+        [Option('d', "domain", Required = true,
+            HelpText = "Web伺服器DNS域名")]
+        public IEnumerable<string> Domains { get; set; }
+        
+        [Option('e',"email",Default = "admin@example.com",
+            HelpText = "管理員電子郵件")]
+        public string Email { get; set; }
+
+        [Option('o', "output", Required = true,
+            HelpText = "輸出PFX檔案路徑(ex. C:\\text.pfx)")]
+        public string OutputFile { get; set; }
+
+        [Option('p', "password", Default = "",
+            HelpText = "輸出PFX檔案密碼")]
+        public string OutputFilePassword { get; set; }
+    }
+}
---
+++ EzCoreKit.LetsEncrypt.Tools/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace EzCoreKit.LetsEncrypt.Tools {
+    class Program {
+        static void Main(string[] args) {
+            var options = GetOptions(args);
+            byte[] pfxBinary = LetsEncrypt.KestrelLetsEncryptExtensions.CreateX509BinaryByLetsEncrypt(
+                null, options.OutputFilePassword, options.Email, options.Domains.ToArray()); 
+
+            BinaryWriter output = new BinaryWriter(new FileStream(options.OutputFile, FileMode.Create));
+            output.Write(pfxBinary);
+            output.Flush();
+            output.BaseStream.Dispose();
+            output.Dispose();
+        }
+
+        static Options GetOptions(string[] args) {
+            try {
+                return (Options)((dynamic)CommandLine.Parser.Default.ParseArguments<Options>(args)).Value;
+            } catch {
+                return null;
+            }
+        }
+    }
+}
