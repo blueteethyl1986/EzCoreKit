bad32107191ebb42450cdb37f109a3a60e95f4bd
XuPeiYao <xupeiyao@nkfust.edu.tw>
2017-09-17 21:43:04 +0800

    加入空集合檢驗擴充方法

---
+++ EzCoreKit/Extensions/IEnumerableExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EzCoreKit.Extensions {
+    public static class IEnumerableExtension {
+        /// <summary>
+        /// 檢查元素數量是否為0
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns>檢查元素數量是否為0</returns>
+        public static bool IsEmpty<TSource>(this IEnumerable<TSource> obj) {
+            return obj.Count() == 0;
+        }
+
+        /// <summary>
+        /// 檢查元素數量是否不為0
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns>檢查元素數量是否不為0</returns>
+        public static bool IsNotEmpty<TSource>(this IEnumerable<TSource> obj) {
+            return !obj.IsEmpty();
+        }
+    }
+}
