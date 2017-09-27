8fa80d78051daf1310216725f2b604d417107468
XuPeiYao <xupeiyao@nkfust.edu.tw>
2017-09-27 20:24:55 +0800

    重構準備，移除2.x專案

--- EzCoreKit.AspNetCore.Ruleflow/Exceptions/AccessInstanceNotFoundException.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace EzCoreKit.AspNetCore.Ruleflow.Exceptions {
-    public class AccessInstanceNotFoundException: Exception {
-        public AccessInstanceNotFoundException() : base("找不到指定appId實體") { }
-    }
-}
--- EzCoreKit.AspNetCore.Ruleflow/Exceptions/AccessInstanceRuleFoundException.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace EzCoreKit.AspNetCore.Ruleflow.Exceptions {
-    public class AccessRuleNotFoundException : Exception {
-        public AccessRuleNotFoundException() : base("找不到指定Rule實體") { }
-    }
-}
