8ce22c66d09f4bcae78087f76dc8d3f1b362a433
徐培堯 <st89382000@gmail.com>
2017-09-28 10:53:29 +0800

    加入EzCoreKit.Threading專案與測試專案

---
+++ EzCoreKit.Test/Threading/TaskHelperTest.cs
@@ -0,0 +1,23 @@
+﻿using EzCoreKit.Threading;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Xunit;
+
+namespace EzCoreKit.Test.Threading {
+    public class TaskHelperTest {
+        [Fact(DisplayName = "Extensions.Threading.LimitedTask")]
+        public void LimitedTask_Test() {
+            Assert.Equal(TaskHelper.LimitedTask(() => {
+
+            }, 1000).GetAwaiter().GetResult(), true);
+            int value = 0;
+            Assert.Equal(TaskHelper.LimitedTask(() => {
+                Thread.Sleep(2000);
+                value = 5;
+            }, 1000).GetAwaiter().GetResult(), false);
+            Assert.Equal(value, 0);
+        }
+    }
+}
