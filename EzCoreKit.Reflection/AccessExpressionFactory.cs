d55de78e2023a67f85c012398605b45c1fe42b79
徐培堯 <st89382000@gmail.com>
2017-09-28 12:10:19 +0800

    加入物件反射擴充方法與AccessExpression工廠類別

---
+++ EzCoreKit.Reflection/AccessExpressionFactory.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace EzCoreKit.Reflection {
+    /// <summary>
+    /// 產生Access Expression工廠類別
+    /// </summary>
+    public static class AccessExpressionFactory {
+        /// <summary>
+        /// 產生存取Func
+        /// </summary>
+        /// <typeparam name="T">存取目標類別</typeparam>
+        /// <param name="name">屬性名稱</param>
+        /// <returns>存取Func</returns>
+        public static Func<T, object> CreateAccessFunc<T>(string name) {
+            return CreateAccessExpressionFunc<T>(name).Compile();
+        }
+
+        /// <summary>
+        /// 產生存取Func
+        /// </summary>
+        /// <typeparam name="T">存取目標類別</typeparam>
+        /// <typeparam name="R">存取結果類別</typeparam>
+        /// <param name="name">屬性名稱</param>
+        /// <returns>存取Func</returns>
+        public static Func<T, R> CreateAccessFunc<T, R>(string name) {
+            return CreateAccessExpressionFunc<T, R>(name).Compile();
+        }
+
+        /// <summary>
+        /// 產生存取Expression Func
+        /// </summary>
+        /// <typeparam name="T">存取目標類別</typeparam>
+        /// <param name="name">屬性名稱</param>
+        /// <returns>存取Expression Func</returns>
+        public static Expression<Func<T, object>> CreateAccessExpressionFunc<T>(string name) {
+            var p = Expression.Parameter(typeof(T), "x");
+            return Expression.Lambda<Func<T, object>>(Expression.Property(p, name), p);
+        }
+
+        /// <summary>
+        /// 產生存取Expression Func
+        /// </summary>
+        /// <typeparam name="T">存取目標類別</typeparam>
+        /// <typeparam name="R">存取結果類別</typeparam>
+        /// <param name="name">屬性名稱</param>
+        /// <returns>存取Expression Func</returns>
+        public static Expression<Func<T, R>> CreateAccessExpressionFunc<T, R>(string name) {
+            var p = Expression.Parameter(typeof(T), "x");
+            return Expression.Lambda<Func<T, R>>(Expression.Property(p, name), p);
+        }
+    }
+}
---
+++ EzCoreKit.Reflection/ObjectReflectionExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace EzCoreKit.Reflection {
+    /// <summary>
+    /// 針對物件反射相關擴充方法
+    /// </summary>
+    public static class ObjectReflectionExtension {
+        /// <summary>
+        /// 取得目前實例指定私有欄位值
+        /// </summary>
+        /// <typeparam name="T">值類別</typeparam>
+        /// <param name="obj">目前實例</param>
+        /// <param name="fieldName">欄位名稱</param>
+        /// <returns>欄位值</returns>
+        public static T GetPrivateFieldValue<T>(this object obj, string fieldName) {
+            TypeInfo temp = obj.GetType().GetTypeInfo();
+            while (temp != typeof(object).GetTypeInfo()) {
+                var fieldInfo = obj.GetType().GetTypeInfo().GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Instance);
+                if (fieldInfo == null) continue;
+                return (T)fieldInfo.GetValue(obj);
+            }
+            return default(T);
+        }
+    }
+}
