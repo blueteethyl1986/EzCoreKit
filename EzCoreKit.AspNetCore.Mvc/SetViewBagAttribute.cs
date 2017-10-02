c27f0439bbc8827683c459e6d61a8e5078cb5d83
徐培堯 <st89382000@gmail.com>
2017-10-02 20:25:09 +0800

    實作ViewData相關設定Attribute

---
+++ EzCoreKit.AspNetCore.Mvc/SetViewBagAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EzCoreKit.AspNetCore.Mvc {
+    /// <summary>
+    /// 執行Action前先行設定ViewBag
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+    public class SetViewBagAttribute : Attribute {
+        /// <summary>
+        /// 鍵值
+        /// </summary>
+        public string Key { get; set; }
+
+        /// <summary>
+        /// 值
+        /// </summary>
+        public object Value { get; set; }
+    }
+}
