7fa27e846800e2fdfc6ffc3073f6e3d051e2d5a3
徐培堯 <st89382000@gmail.com>
2017-10-02 20:07:09 +0800

    實作IConfiguration轉動態物件擴充方法

---
+++ EzCoreKit.AspNetCore/IConfigurationSectionExtension.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+
+namespace EzCoreKit.AspNetCore {
+    /// <summary>
+    /// 針對<see cref="IConfigurationSection"/>的擴充方法
+    /// </summary>
+    public static class IConfigurationSectionExtension {
+        /// <summary>
+        /// 將IConfiguration物件轉換為<see cref="ExpandoObject"/>物件
+        /// </summary>
+        /// <param name="configureation">IConfiguration實例</param>
+        /// <returns><see cref="ExpandoObject"/>物件</returns>
+        public static object ToDynamicObject(this IConfiguration configureation) {
+            if (configureation == null) return null;
+
+            dynamic result = new ExpandoObject();
+            IDictionary<string, object> setting = (IDictionary<string, object>)result;
+
+            var attributes = configureation.GetChildren();
+
+            var firstKey = attributes.FirstOrDefault()?.Key;
+            int temp = 0;
+            if (int.TryParse(firstKey, out temp)) {
+                result = new object[attributes.Count()];
+                foreach (var attr in attributes) {
+                    if (attr.GetChildren().Count() > 0) {
+                        result[temp++] = attr.ToDynamicObject();
+                    } else {
+                        result[temp++] = attr.Value?.Length > 0 ? attr.Value : null;
+                    }
+                }
+            } else {
+                foreach (var attr in attributes) {
+                    if (attr.GetChildren().Count() > 0) {
+                        setting[attr.Key] = attr.ToDynamicObject();
+                    } else {
+                        setting[attr.Key] = attr.Value?.Length > 0 ? attr.Value : null;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
