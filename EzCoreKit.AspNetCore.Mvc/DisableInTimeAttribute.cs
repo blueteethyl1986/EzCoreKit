c1b4a41a23e82f686aa29ba09731f3cdb036a1a4
徐培堯 <st89382000@gmail.com>
2017-10-02 20:24:40 +0800

    實作Action限制Attribute

---
+++ EzCoreKit.AspNetCore.Mvc/DisableInTimeAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EzCoreKit.AspNetCore.Mvc {
+    /// <summary>
+    /// 標誌控制器或方法在指定期間內停用，其餘時間啟用(大於等於Start並且小於End)
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+    public class DisableInTimeAttribute : Attribute {
+        /// <summary>
+        /// 起始時間
+        /// </summary>
+        public DateTime Start { get; set; }
+
+        /// <summary>
+        /// 結束時間
+        /// </summary>
+        public DateTime End { get; set; }
+
+        public DisableInTimeAttribute(string StartTime, string EndTime) {
+            Start = DateTime.Parse(StartTime);
+            End = DateTime.Parse(EndTime);
+        }
+    }
+}
---
+++ EzCoreKit.AspNetCore.Mvc/EnableInTimeAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EzCoreKit.AspNetCore.Mvc {
+    /// <summary>
+    /// 標誌控制器或方法在指定期間內啟用，其餘時間停用(大於等於Start並且小於End)
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+    public class EnableInTimeAttribute : Attribute {
+        /// <summary>
+        /// 起始時間
+        /// </summary>
+        public DateTime Start { get; set; }
+
+        /// <summary>
+        /// 結束時間
+        /// </summary>
+        public DateTime End { get; set; }
+
+        public EnableInTimeAttribute(string StartTime, string EndTime) {
+            Start = DateTime.Parse(StartTime);
+            End = DateTime.Parse(EndTime);
+        }
+    }
+}
