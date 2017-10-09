d10273540d11dac0efdc30c5635a0f00630cb0ac
徐培堯 <st89382000@gmail.com>
2017-10-09 13:15:06 +0800

    加入基本RestClientBuilder

---
+++ EzCoreKit.Rest/RestClientBuilder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EzCoreKit.Rest {
+    /// <summary>
+    /// 為RESTful Web API Client建構器
+    /// </summary>
+    public class RestClientBuilder<T> {
+        private string baseUri { get; set; }
+
+        public RestClienBuilder() {
+            if (!typeof(T).IsInterface) {//必須是interface
+                throw new ArgumentException($"{nameof(T)}應為interface");
+            }
+        }
+
+        /// <summary>
+        /// 設定基底Uri
+        /// </summary>
+        /// <param name="uriString">Uri實例</param>
+        /// <returns>為RESTful Web API Client建構器</returns>
+        public RestClienBuilder<T> SetBaseUri(Uri uri) {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 設定基底Uri
+        /// </summary>
+        /// <param name="uriString">Uri字串</param>
+        /// <returns>為RESTful Web API Client建構器</returns>
+        public RestClienBuilder<T> SetBaseUri(string uriString) {
+            return SetBaseUri(new Uri(uriString));
+        }
+
+        /// <summary>
+        /// 使用目前建構器的設定產生RESTful Web API Client實例
+        /// </summary>
+        /// <returns>RESTful Web API Client實例</returns>
+        public T Build() {
+            throw new NotImplementedException();
+        }
+    }
+}
