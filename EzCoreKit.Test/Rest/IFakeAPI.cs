d88c7503b707db314649e8f360958a789a4d49e0
徐培堯 <st89382000@gmail.com>
2017-10-13 22:32:21 +0800

    加入RestClientBuilder實例產生測試

---
+++ EzCoreKit.Test/Rest/IFakeAPI.cs
@@ -0,0 +1,14 @@
+﻿using EzCoreKit.Rest.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EzCoreKit.Test.Rest {
+    [RestBaseUri("https://jsonplaceholder.typicode.com/")]
+    public interface IFakeAPI {
+        [RestMethod(
+            Uri = "http://opendata.cwb.gov.tw/govdownload?dataid=E-A0015-001R&authorizationkey=rdec-key-123-45678-011121314")]
+        Task<string> GetEarthquakesLocation();
+    }
+}
---
+++ EzCoreKit.Test/Rest/RestClientBuilderTest.cs
@@ -0,0 +1,16 @@
+﻿using EzCoreKit.Rest;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace EzCoreKit.Test.Rest {
+    public class RestClientBuilderTest {
+        [Fact(DisplayName = "Rest.CreateInstance")]
+        public async void RestClientBuilder_CreateInstanceTest() {
+            RestClientBuilder<IFakeAPI> builder = new RestClientBuilder<IFakeAPI>();
+            var obj = builder.Build();
+            Assert.NotNull(obj);
+        }
+    }
+}
