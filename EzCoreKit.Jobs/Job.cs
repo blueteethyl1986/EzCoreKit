7b6bd715153a802c355c19a91b6c793a0286ef9c
徐培堯 <st89382000@gmail.com>
2017-10-18 22:02:34 +0800

    匯入Yay專案改EzCoreKit.Jobs外部工作

---
+++ EzCoreKit.Jobs/Job.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace EzCoreKit.Jobs {
+    /// <summary>
+    /// 工作
+    /// </summary>
+    public class Job {
+        /// <summary>
+        /// 標準輸入
+        /// </summary>
+        internal StreamWriter standardInput;
+
+        /// <summary>
+        /// 標準輸出
+        /// </summary>
+        internal StreamReader standardOutput;
+
+        /// <summary>
+        /// Process實例
+        /// </summary>
+        internal Process process;
+
+        /// <summary>
+        /// 唯一識別號
+        /// </summary>
+        public Guid Id { get; private set; } = Guid.NewGuid();
+
+        /// <summary>
+        /// 命令
+        /// </summary>
+        public string Command { get; set; }
+
+        /// <summary>
+        /// 命令參數
+        /// </summary>
+        public string Arguments { get; set; }
+
+        /// <summary>
+        /// 工作進度，0~100
+        /// </summary>
+        public int Processing { get; protected set; }
+
+        /// <summary>
+        /// 工作是否已結束
+        /// </summary>
+        public bool HasExited => process.HasExited;
+
+        /// <summary>
+        /// 執行結果，0表示正常結束。
+        /// </summary>
+        public int ExitCode => process.ExitCode;
+
+
+
+        public void Input(string text) {
+            standardInput.Write(text);
+        }
+
+        public void InputLine(string text) {
+            standardInput.WriteLine(text);
+        }
+
+        public virtual void OnOutput(string line) { }
+    }
+}
---
+++ EzCoreKit.Jobs/JobManager.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EzCoreKit.Jobs {
+    /// <summary>
+    /// 工作管理者
+    /// </summary>
+    public class JobManager {
+        /// <summary>
+        /// 工作清單
+        /// </summary>
+        private static List<Job> jobList = new List<Job>();
+
+        /// <summary>
+        /// 工作清單
+        /// </summary>
+        public static IReadOnlyList<Job> Jobs => jobList.AsReadOnly();
+
+        /// <summary>
+        /// 當有工作被停止
+        /// </summary>
+        public static event EventHandler Exited;
+
+        /// <summary>
+        /// 加入新工作
+        /// </summary>
+        /// <param name="job"></param>
+        public static void Add(Job job) {
+            jobList.Add(job);
+        }
+
+        /// <summary>
+        /// 執行工作
+        /// </summary>
+        /// <param name="id">唯一識別號</param>
+        public static void Run(Guid id) {
+            var job = Get(id);
+            var process = new Process {
+                StartInfo = new ProcessStartInfo {
+                    FileName = job.Command,
+                    Arguments = job.Arguments,
+                    UseShellExecute = false,
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+
+            job.process = process;
+            job.process.Exited += Process_Exited;
+            lock (jobList) {
+                jobList.Add(job);
+                process.Start();
+                job.standardOutput = process.StandardOutput;
+                job.standardInput = process.StandardInput;
+            }
+            Task.Run(() => {
+                while (!process.StandardOutput.EndOfStream) {
+                    job.OnOutput(process.StandardOutput.ReadLine());
+                }
+            }, CancellationToken.None);
+        }
+
+        /// <summary>
+        /// 加入並執行新工作
+        /// </summary>
+        /// <param name="job">工作</param>
+        public static void AddAndRun(Job job) {
+            Add(job);
+            Run(job.Id);
+        }
+
+        /// <summary>
+        /// 取消執行中的工作
+        /// </summary>
+        /// <param name="id">唯一識別號</param>
+        public static void Cancel(Guid id) {
+            var target = Get(id);
+            if (target.HasExited) target.process.Kill();
+        }
+
+        /// <summary>
+        /// 取消執行中的工作並從工作清單移除
+        /// </summary>
+        /// <param name="id">唯一識別號</param>
+        public static void RemoveAndCancel(Guid id) {
+            var target = Get(id);
+            if (target.HasExited) target.process.Kill();
+            jobList.Remove(target);
+        }
+
+        /// <summary>
+        /// 取得指定工作實例
+        /// </summary>
+        /// <param name="id">唯一識別號</param>
+        /// <returns>工作實例</returns>
+        public static Job Get(Guid id) {
+            return Jobs.First(x => x.Id == id);
+        }
+
+        private static void Process_Exited(object sender, EventArgs e) {
+            JobManager.Exited?.Invoke(sender, e);
+        }
+    }
+}
