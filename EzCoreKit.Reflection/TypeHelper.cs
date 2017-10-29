a9520436629daadb5cc596759c4570da5f48ef9b
徐培堯 <st89382000@gmail.com>
2017-10-29 15:10:21 +0800

    修正取得namespace所有type方法錯誤

--- EzCoreKit.Reflection/TypeHelper.cs
+++ EzCoreKit.Reflection/TypeHelper.cs
@@ -9,14 +9,28 @@ namespace EzCoreKit.Reflection {
     /// 類型幫助類別
     /// </summary>
     public static class TypeHelper {
-        /// <summary>
+        /// <summary>Assembly.GetCallingAssembly()
         /// 取得指定命名空間內所有類型陣列
         /// </summary>
         /// <param name="ns">指定命名空間</param>
         /// <returns>指定命名空間內所有類型陣列</returns>
         public static Type[] GetNamespaceTypes(string ns) {
-            return (from t in Assembly.GetExecutingAssembly().GetTypes()
-                    where t.IsClass && t.Namespace == ns
+            var types = Assembly.GetExecutingAssembly().GetTypes()
+                .Concat(Assembly.GetCallingAssembly().GetTypes());
+            return (from t in types
+                    where t.Namespace == ns
+                    select t).ToArray();
+        }
+
+        /// <summary>
+        /// 取得指定Assembly中目標命名空間內所有類型陣列
+        /// </summary>
+        /// <param name="assembly">指定Assembly</param>
+        /// <param name="ns">指定命名空間</param>
+        /// <returns>指定命名空間內所有類型陣列</returns>
+        public static Type[] GetNamespaceTypes(Assembly assembly, string ns) {
+            return (from t in assembly.GetTypes()
+                    where t.Namespace == ns
                     select t).ToArray();
         }
     }
---
+++ EzCoreKit.Test/Reflection/TypeHelperExtension_Test.cs
@@ -0,0 +1,18 @@
+﻿using EzCoreKit.Test.TestModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using EzCoreKit.Reflection;
+using Xunit;
+using System.Linq;
+
+namespace EzCoreKit.Test.Reflection {
+    public class TypeHelperExtension_Test {
+        [Fact(DisplayName = "Reflection.GetNamespaceTypes")]
+        public void GetNamespaceTypes() {
+            var types = TypeHelper.GetNamespaceTypes("EzCoreKit.Test.TestModels")
+                .Where(x => x.IsClass);
+            Assert.NotEmpty(types);
+        }
+    }
+}
