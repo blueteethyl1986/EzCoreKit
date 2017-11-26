123deb5823f34abff2f8a69ebe35cb60f938dcb0
XuPeiYao <st89382000@gmail.com>
2017-11-26 14:31:33 +0800

    加入HttpClient JSON擴充擴充方法

---
+++ EzCoreKit.Extensions/HttpClientExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace EzCoreKit.Extensions {
+    /// <summary>
+    /// 針對<see cref="HttpClient"/>相關功能擴充方法
+    /// </summary>
+    public static class HttpClientExtension {
+        /// <summary>
+        /// 將 GET 要求傳送至指定的 URI，並透過非同步作業，以Json形式傳回回應內容
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="requestUri">傳送要求的目標 URI</param>
+        /// <returns>操作結果</returns>
+        public static async Task<JToken> GetJsonAsync(this HttpClient client, string requestUri) {
+            return JToken.Parse(await client.GetStringAsync(requestUri));
+        }
+
+        /// <summary>
+        /// 將 GET 要求傳送至指定的 URI，並透過非同步作業，以Json形式傳回回應內容
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="requestUri">傳送要求的目標 URI</param>
+        /// <returns>操作結果</returns>
+        public static async Task<JToken> GetJsonAsync(this HttpClient client, Uri requestUri) {
+            return JToken.Parse(await client.GetStringAsync(requestUri));
+        }
+    }
+}
