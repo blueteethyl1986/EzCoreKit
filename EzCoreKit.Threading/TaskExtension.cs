684a6a2da8230cb6f11e9afa644735e175f2d808
XuPeiYao <st89382000@gmail.com>
2017-12-02 22:54:14 +0800

    加入無參數Task之ToSync方法

--- EzCoreKit.Threading/TaskExtension.cs
+++ EzCoreKit.Threading/TaskExtension.cs
@@ -17,5 +17,14 @@ namespace EzCoreKit.Threading {
         public static T ToSync<T>(this Task<T> task) {
             return task.GetAwaiter().GetResult();
         }
+
+        /// <summary>
+        /// 將非同步方法轉換為同步並取得執行結果
+        /// </summary>
+        /// <param name="task">非同步程序實例</param>
+        /// <returns>方法執行結果</returns>
+        public static void ToSync(this Task task) {
+            task.GetAwaiter().GetResult();
+        }
     }
 }
